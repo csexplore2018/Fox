1702ec9ec79233a1925ecdfaa2ef51c9c641aa87
gc_science5 <>
2018-06-28 10:19:57 -0400

    initial commit

---
+++ src/fox/Fox.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fox;
+
+/**
+ *
+ * @author gc_science5
+ */
+public class Fox {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
