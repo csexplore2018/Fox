59053d686a44d846598e9f028d16a2894f67426d
gc_science5 <gc_science5@SE137LAB-04.ad.svsu.edu>
2018-06-28 10:44:57 -0400

    next commit

--- src/fox/Fox.java
+++ src/fox/Fox.java
@@ -11,11 +11,39 @@ package fox;
  */
 public class Fox {
 
+    private String name = "Fox";
+    public String food = "Mice";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Animal a = new Animal();
+        System.out.println(a.name);
+        a.talk();
+        
+        Vixen v = new Vixen();
+        System.out.println(v.name);
+        v.talk();
     }
     
+}
+
+class Animal{
+    public String name = "Animal";
+    public String food = "Food";
+    
+    public void talk(){
+        System.out.println("Hey...");
+    }
+}
+
+class Vixen extends Animal{
+    public String name = "Vixen";
+    public String food = "Mice";
+    
+    public void talk(){
+        System.out.println("Ring-ding-ding-ding-dingeringeding!");
+    }
 }
